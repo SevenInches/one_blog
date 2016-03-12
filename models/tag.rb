ff05f590f63cbe1bebcf2b474eb3093ab6dbb71d
sevenInch <lijinghao333@aliyun.com>
2016-03-12 22:17:13 +0800

    modify tag limit

--- models/tag.rb
+++ models/tag.rb
@@ -2,6 +2,6 @@ class Tag < ActiveRecord::Base
 	has_one :blog
 
 	def self.find_tags
-  		Tag.limit(12).all().collect { |tag| [tag.tag_name, tag.id] }
+  		Tag.all().collect { |tag| [tag.tag_name, tag.id] }
 	end  
 end
